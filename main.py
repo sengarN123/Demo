0840edc13e4c0e5876c9aaca09bc2e0dc24f18bf
Nandani <sengarnandani91@gmail.com>
2026-03-24 12:44:54 +0530

    arm

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
 print("hello world")
 print("hey")
+
+for i in range(1,501):
+    count = 0
+    q = i
+    rem = 0
+    mul = 0
+    result = 0
+
+    while q != 0:
+        q = q // 10
+        count +=1
+
+    q = i
+
+    while q!=0:
+        rem = q % 10
+        mul = rem ** count
+        result = result + mul
+        q = q // 10
+
+    if result == i:
+        print(f"{i} Is Armstrong")
+    else:
+        print(f"{i} is Not Armstrong")
